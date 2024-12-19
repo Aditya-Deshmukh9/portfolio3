bf4f30e9e869baf517c6eba2ba5f3003127d15b4
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-12-19 22:28:29 +0530

    navbar setIsScrollStoped(false); // Reset scroll-stopped status on every scroll even

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -34,30 +34,23 @@ function About() {
       >
         {ABOUT.text2}
       </motion.h3>
-      <div className="p-5 flex items-center justify-center">
+      <div className="flex items-center justify-center p-5">
         <Link
           href="https://drive.google.com/file/d/1e7NPwjcQLUJVumEgxkUCiUNcIg7PlyuB/view?usp=sharing"
           target="_blank"
-          className="group p-5 cursor-pointer relative text-xl font-normal border-0 flex items-center justify-center
-        bg-transparent text-indigo-500 h-auto w-[170px] overflow-hidden transition-all duration-100"
+          className="group relative flex h-auto w-[170px] cursor-pointer items-center justify-center overflow-hidden border-0 bg-transparent p-5 text-xl font-normal text-indigo-500 transition-all duration-100"
         >
-          <span className="group-hover:w-full absolute left-0 h-full w-5 border-y border-l border-indigo-500 transition-all duration-500"></span>
+          <span className="absolute left-0 h-full w-5 border-y border-l border-indigo-500 transition-all duration-500 group-hover:w-full"></span>
 
-          <p
-            className="group-hover:opacity-0 group-hover:translate-x-[-100%] absolute translate-x-0 transition-all
-          duration-200"
-          >
+          <p className="absolute translate-x-0 transition-all duration-200 group-hover:translate-x-[-100%] group-hover:opacity-0">
             Resume
           </p>
 
-          <span
-            className="group-hover:translate-x-0  group-hover:opacity-100 absolute bg-indigo-400
-           translate-x-full opacity-0 text-white w-full p-5 transition-all duration-200"
-          >
+          <span className="absolute w-full translate-x-full bg-indigo-400 p-5 text-center text-white opacity-0 transition-all duration-200 group-hover:translate-x-0 group-hover:opacity-100">
             Thank you!
           </span>
 
-          <span className="group-hover:w-full absolute right-0 h-full w-5  border-y border-r  border-indigo-500 transition-all duration-500"></span>
+          <span className="absolute right-0 h-full w-5 border-y border-r border-indigo-500 transition-all duration-500 group-hover:w-full"></span>
         </Link>
       </div>
     </div>
