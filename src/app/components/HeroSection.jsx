e5994ee3e782eac72ba1ed4821c7f94256a6a13a
Aditya-Deshmukh9 <adityadesh937@gmail.com>
2024-12-19 23:44:02 +0530

    home text pbug solve

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -34,19 +34,10 @@ function HeroSection() {
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: 1 }}
       >
-        {/*<span
-          className={`mb-3 text-center text-5xl font-extrabold italic text-white/70 sm:mb-0 sm:text-left md:text-7xl xl:text-[5rem]`}
-        >
-          <span className="relative">
-            <div className="absolute -right-3 bottom-[1.1rem] hidden h-[4.5rem] w-3.5 rotate-[14deg] bg-gradient-to-r from-transparent from-60% via-[#f7d486] to-[#f27a7d] md:inline"></div>
-            <div className="absolute -right-[3px] bottom-4 hidden h-1 w-full bg-gradient-to-r from-transparent from-20% via-[#f7d486] to-[#f27a7d] md:inline"></div>
-            frontend
-          </span>
-        </span> */}
-        <h2 className="font-protest bg-gradient-to-r from-[#c5f9d7] via-[#7ebf77] to-[#3ACD45] bg-clip-text text-5xl font-semibold uppercase tracking-wide text-transparent md:text-7xl">
+        <h2 className="text-5xl font-semibold uppercase tracking-wide md:text-7xl">
           {PROFILE.name}
         </h2>
-        <p className="pt-2 text-[14px] font-semibold tracking-normal text-[#7ebf77]">
+        <p className="pt-2 text-[14px] font-semibold tracking-normal">
           {PROFILE.info}
         </p>
       </motion.div>
