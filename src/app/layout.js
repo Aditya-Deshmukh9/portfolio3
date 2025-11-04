33bebb5dc632d0a53c46c419ba1910cda0520267
Aditya Deshmukh <143163173+Aditya-Deshmukh9@users.noreply.github.com>
2025-11-04 20:13:56 +0530

    google analytics script add

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+     <!-- Google tag (gtag.js) -->
+     <script async src="https://www.googletagmanager.com/gtag/js?id=G-KWFQ3B04WP"></script>
+      <script>
+       window.dataLayer = window.dataLayer || [];
+       function gtag(){dataLayer.push(arguments);}
+       gtag('js', new Date());
+
+        gtag('config', 'G-KWFQ3B04WP');
+      </script>
       <body className={`${inter.className} ${roboto.className} bg-black tracking-tighter text-gray-200 antialiased scroll-smooth`}>
         <SmoothScroll>
           <Navbar />
